5e0852344e8e6fb473e5995028f34041cb8357c4
najib2003 <z.najib4239@uca.ac.ma>
2025-09-27 19:12:43 +0100

    signals

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,8 +11,7 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 export class UserComponent {
  selectedUser = signal(DUMMY_USERS[randomIndex]);
  imagePath =computed(() => 'assets/users/' + this.selectedUser().avatar);
-//  get imagePath() {
-//   return 'assets/users/' + this.selectedUser.avatar}
+
 
   onSelectUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
