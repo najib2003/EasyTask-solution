00242d1f7c4a869e48ff2c1387bdeb3be9a1ee6c
najib2003 <z.najib4239@uca.ac.ma>
2025-09-28 21:39:40 +0100

    started to get on my nervs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+
+export class AppComponent {
+
+  onSelectUser( id: string) {
+    this.selectedUserId = id;
+
+  }
+  selectedUserId = 'U1';
+  users = DUMMY_USERS;
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,4 @@
-import { Component , computed, signal} from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, computed, EventEmitter, Input, input, output, Output} from '@angular/core';
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -9,13 +7,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
- selectedUser = signal(DUMMY_USERS[randomIndex]);
- imagePath =computed(() => 'assets/users/' + this.selectedUser().avatar);
+  @Input({required:true}) id!: string;
+  @Input({required:true}) avatar!: string;
+  @Input({required:true}) name!: string;
+  @Output() select = new EventEmitter();
 
+get imagePath() {
+    return './assets/users/' + this.avatar;
+  }
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    this .select.emit(this.id);
   }
  }
 
